24b13d18bde0a0cf2d075a5ad3b6c76a25a17a9e
pietre2023 <127364871+pietre2023@users.noreply.github.com>
2023-03-09 03:12:17 +0100

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt install swig python-dev gcc-arm-linux-gnueabihf bison flex make python3-setuptools libssl-dev u-boot-tools device-tree-compiler
+sudo apt install -y swig python-dev gcc-arm-linux-gnueabihf bison flex make python3-setuptools libssl-dev u-boot-tools device-tree-compiler
 git clone git://git.denx.de/u-boot.git
 cd u-boot
 echo "      Menu de compilación del u-boot"
