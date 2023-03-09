e1137236445cb0f4133b7a8aef3b961a0852aed9
pietre2023 <127364871+pietre2023@users.noreply.github.com>
2023-03-09 03:06:47 +0100

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-sudo apt install swig python-dev gcc-arm-linux-gnueabihf bison flex make python3-setuptools libssl-dev
+sudo apt install swig python-dev gcc-arm-linux-gnueabihf bison flex make python3-setuptools libssl-dev u-boot-tools device-tree-compiler
+git clone git://git.denx.de/u-boot.git
+cd u-boot
 make CROSS_COMPILE=aarch64-linux-gnu-
