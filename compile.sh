2f2b0e9bd67a567bf94993ec32fb36574e316b9a
pietre2023 <127364871+pietre2023@users.noreply.github.com>
2023-03-09 02:59:24 +0100

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo apt install swig python-dev gcc-arm-linux-gnueabihf bison flex make python3-setuptools
+sudo apt install swig python-dev gcc-arm-linux-gnueabihf bison flex make python3-setuptools libssl-dev
 make CROSS_COMPILE=aarch64-linux-gnu-
